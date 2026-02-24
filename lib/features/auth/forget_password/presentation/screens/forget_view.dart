0b93e8b3aab82114bf684b96443d56c075f4b1ee
Ziad <zslah2800@gmail.com>
2026-02-25 00:30:53 +0200

    refactor in forget password

--- lib/features/auth/forget_password/presentation/screens/forget_view.dart
+++ lib/features/auth/forget_password/presentation/screens/forget_view.dart
@@ -1,52 +1,12 @@
-import 'package:blood_donation/core/utils/app_text_styles.dart';
-import 'package:blood_donation/core/widgets/custom_button.dart';
-import 'package:blood_donation/core/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../core/utils/validators.dart';
-import '../../../../../generated/l10n.dart';
+import '../widgets/forget_password_view_body.dart';
 
 class ForgetPasswordView extends StatelessWidget {
   const ForgetPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 17),
-        child: ListView(
-          children: [
-            const SizedBox(height: 150),
-            Text(
-              S.of(context).areForgPass,
-              style: AppTextStyles.b32(context),
-              textAlign: .center,
-            ),
-            const SizedBox(height: 16),
-            Text(
-              S.of(context).forgPassValidate,
-              style: AppTextStyles.r20(context),
-              textAlign: .center,
-            ),
-            const SizedBox(height: 48),
-            CustomTextField(
-              controller: TextEditingController(),
-              label: S.of(context).email,
-              hint: S.of(context).entrEmail,
-              keyboardType: TextInputType.emailAddress,
-              validator: emailValidator,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-            ),
-            const SizedBox(height: 200),
-            Column(
-              crossAxisAlignment: .stretch,
-              children: [
-                CustomButton(onPressed: () {}, label: S.of(context).sendOtp),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Scaffold(body: ForgetPasswordViewBody());
   }
 }
---
+++ lib/features/auth/forget_password/presentation/widgets/forget_password_view_body.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/app_text_styles.dart';
+import '../../../../../core/utils/validators.dart';
+import '../../../../../core/widgets/custom_button.dart';
+import '../../../../../core/widgets/custom_text_field.dart';
+import '../../../../../generated/l10n.dart';
+
+class ForgetPasswordViewBody extends StatelessWidget {
+  const ForgetPasswordViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 17),
+      child: ListView(
+        children: [
+          const SizedBox(height: 150),
+          Text(
+            S.of(context).areForgPass,
+            style: AppTextStyles.b32(context),
+            textAlign: .center,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            S.of(context).forgPassValidate,
+            style: AppTextStyles.r20(context),
+            textAlign: .center,
+          ),
+          const SizedBox(height: 48),
+          CustomTextField(
+            controller: TextEditingController(),
+            label: S.of(context).email,
+            hint: S.of(context).entrEmail,
+            keyboardType: TextInputType.emailAddress,
+            validator: emailValidator,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+          ),
+          const SizedBox(height: 200),
+          Column(
+            crossAxisAlignment: .stretch,
+            children: [
+              CustomButton(onPressed: () {}, label: S.of(context).sendOtp),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
